8be2bc9d3f44c2361e416af33a2fd0b806df8682
Sammed Teerth <sammed@aeriesys.com>
2024-12-24 16:53:58 +0530

    RegisterUser Mail

---
+++ laravel/app/Mail/RegistrationEmail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class RegistrationEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $emailData;
+
+    public function __construct($emailData)
+    {
+        $this->emailData = $emailData;
+    }
+
+    public function build()
+    {
+        return $this->subject('Confirm Your Email Address - BidsPortal')
+                    ->view('emails.RegisteredUser')
+                    ->with('data', $this->emailData);
+    }
+}
