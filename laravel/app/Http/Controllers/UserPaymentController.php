5d2dd8881ef9a295e3a186039ced147f9a74a225
Sammed Teerth <sammed@aeriesys.com>
2024-12-25 15:41:49 +0530

    PaymentReciept

--- laravel/app/Http/Controllers/UserPaymentController.php
+++ laravel/app/Http/Controllers/UserPaymentController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 use App\Models\UserPayment;
+use App\Models\UserSubscription;
 use Illuminate\Http\Request;
 use App\Http\Resources\UserPaymentResource;
 use Auth;
+use PDF;
 
 class UserPaymentController extends Controller
 {
@@ -19,5 +21,28 @@ class UserPaymentController extends Controller
         ]);
     	$user_payments = UserPayment::where('user_id', $request->user_id)->get();
 		return UserPaymentResource::collection($user_payments);
+    }
+
+    public function generateSubscriptionPdf(Request $request)
+    {
+        // $request->validate([
+        //     'user_id' => 'required|exists:users,user_id'
+        // ]);
+
+        // $subscriptions = UserSubscription::where('user_id', $request->user_id)->where('txn_id', $request->txn_id)->orderBy('created_at', 'desc')->first();
+        $subscriptions = UserSubscription::where('user_id', $request->user_id)->orderBy('created_at', 'desc')->first();
+        $data = [
+            'user' => $subscriptions->User->name,
+            'amount' => $subscriptions->payment_gross,
+            'payment_type' => $subscriptions->payment_method,
+            'valid_upto' => $subscriptions->valid_to,
+            // 'user' => "Sammed Teerth",
+            // 'amount' => 365,
+            // 'payment_type' => "Online",
+            // 'valid_upto' => "2025-01-25",
+        ];
+
+        $pdf = PDF::loadView('subscription_pdf', $data);
+        return $pdf->download('subscription_receipt.pdf');
     }
 }
