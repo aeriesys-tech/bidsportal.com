396ff1648286c93774e3f0c63f8b43a77a7e8faf
Ajit Kundgol <ajit@aeriesys.com>
2025-01-29 11:08:17 +0530

    naics_code

--- laravel/app/Http/Controllers/NaicsController.php
+++ laravel/app/Http/Controllers/NaicsController.php
@@ -13,30 +13,78 @@ class NaicsController extends Controller
     public function getNaics(Request $request)
     {
         if($request->search){
-            $parent_results = Naics::whereNull('naics_parent_id')
-            ->where(function ($query) use ($request) {
+            // $parent_results = Naics::whereNull('naics_parent_id')
+            // ->where(function ($query) use ($request) {
+            //     $query->where('naics_code', 'like', '%' . $request->search . '%')
+            //           ->orWhere('naics_description', 'like', '%' . $request->search . '%');
+            // })
+            // ->pluck('naics_id');
+
+            $search_results = Naics::where(function ($query) use ($request) {
                 $query->where('naics_code', 'like', '%' . $request->search . '%')
                       ->orWhere('naics_description', 'like', '%' . $request->search . '%');
             })
-            ->pluck('naics_id');
-            $child_results = Naics::where(function ($query) use ($request) {
-                $query->where('naics_code', 'like', '%' . $request->search . '%')
-                      ->orWhere('naics_description', 'like', '%' . $request->search . '%');
-            })
-            ->select('naics_parent_id')
-            ->whereNotNull('naics_parent_id')
-            ->distinct()
-            ->pluck('naics_parent_id');
+            ->select('naics_code')
+            ->pluck('naics_code');
 
-            $all_parents = $parent_results->merge($child_results)->unique('naics_id')->values();
+$additional_codes = []; // Array to store additional codes to add to $search_results
+$search_results_array = is_array($search_results) ? $search_results : $search_results->toArray();
+$naics_codes = [];
 
-            $all_parent_codes = $parent_results->merge($child_results);
+foreach ($search_results_array as $code) {
+    // Check the start of the string and add related codes
+    if (str_starts_with($code, "32") || str_starts_with($code, "33")) {
+        if (!in_array('31-33', $search_results_array)) {
+            $search_results_array[] = "31-33"; // Add 31
+        }
+        if (!in_array('31-33', $naics_codes)) {
+            $naics_codes[] = "31-33";
+        }
+    } elseif (str_starts_with($code, "45")) {
+        if (!in_array('44-45', $search_results_array)) {
+            $search_results_array[] = "44-45"; // Add 31
+        }
+        if (!in_array('44-45', $naics_codes)) {
+            $naics_codes[] = "44-45"; // Add 44
+        }
+    } elseif (str_starts_with($code, "49")) {
+        if (!in_array('48-49', $search_results_array)) {
+            $search_results_array[] = "48-49"; // Add 31
+        }
+        if (!in_array('48-49', $naics_codes)) {
+            $naics_codes[] = "48-49"; // Add 48
+        }
+    }else{
+        if (!in_array($code, $naics_codes)) {
+            $naics_codes[] = $code;     
+        }
+    }
+    $naics_codes[] = substr($code, 0, 2);
+    $naics_codes[] = substr($code, 0, 3);
+    $naics_codes[] = substr($code, 0, 4);
+}
 
-            $naics_codes = Naics::whereIn('naics_id', $all_parent_codes)->pluck('naics_code');
+// Merge additional codes into search_results
+// $search_results = array_merge($search_results, $additional_codes);
+
+// Build the query
+$childe_naics_ids = Naics::whereNull('naics_parent_id')
+    ->where(function ($query) use ($search_results_array) {
+        foreach ($search_results_array as $code) {
+            $query->orWhereRaw('naics_code LIKE ?', [substr($code, 0, 2) . '%']);
+        }
+    })
+    ->pluck('naics_id');
+
+            // $all_parents = $parent_results->merge($childe_naics_ids)->unique('naics_id')->values();
+
+            // $all_parent_codes = $parent_results->merge($child_results);
+
+            // $naics_codes = Naics::whereIn('naics_id', $all_parent_codes)->pluck('naics_code');
 
 
             $naics = Naics::select('naics_id', 'naics_code', 'naics_description')
-                    ->whereIn('naics_id', $all_parents)->whereNull('naics_parent_id')->get();
+                    ->whereIn('naics_id', $childe_naics_ids)->whereNull('naics_parent_id')->get();
 
         }else{
             $cacheKey = 'naics';
@@ -45,7 +93,7 @@ class NaicsController extends Controller
                           ->whereNull('naics_parent_id')
                           ->get();
             });
-            $naics_codes = [];
+            $search_results = [];
         }
         $naics_resource = NaicsResource::collection($naics); 
         return (['naics' => $naics_resource, 'naics_codes' => $naics_codes]);
