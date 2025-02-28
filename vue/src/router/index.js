fce10508f95963717e142f0997411f525e75bb4c
Ajit Kundgol <ajit@aeriesys.com>
2025-02-28 11:24:19 +0530

    hashremoved

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import Federal from "@/views/bids/Federal.vue";
 import State from "@/views/bids/State.vue";
@@ -330,7 +330,7 @@ const routes = [
 ];
 
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHashHistory(),
 	routes,
 });
 
