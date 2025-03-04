64acaad3229c89d14e00e9118aa44820ba333f07
Ajit Kundgol <ajit@aeriesys.com>
2025-03-04 18:09:20 +0530

    olddbdump

--- laravel/app/Http/Controllers/DatabaseController.php
+++ laravel/app/Http/Controllers/DatabaseController.php
@@ -11,7 +11,7 @@ class DatabaseController extends Controller
 {
     public function transferFederalTenders()
     {
-        DB::connection('target_db')->table('tbl_tender')->where('tdr_region', 2)
+        DB::connection('bidsp4h0_bids_n')->table('tbl_tender')->where('tdr_region', 2)
         ->orderBy('tdr_no')->chunk(100, function ($tenders) {
             $jobData = $tenders->map(function ($tbl) {
                 return [
@@ -51,7 +51,7 @@ class DatabaseController extends Controller
 
     public function transferStateTenders()
     {
-        DB::connection('target_db')->table('tbl_tender')->where('tdr_region', 1)
+        DB::connection('bidsp4h0_bids_n')->table('tbl_tender')->where('tdr_region', 1)
         ->orderBy('tdr_no')->chunk(100, function ($tenders) {
             $jobData = $tenders->map(function ($tbl) {
                 return [
@@ -96,63 +96,67 @@ class DatabaseController extends Controller
     {
         $users = DB::connection('bidsp4h0_bids_n')->table('tbl_users')->get();
         foreach ($users as $key => $user) {
-            Log::info($user);
-            // $user_data = DB::connection('mysql')->table('users')->create([
-            //     'name' => 
-            //     'email' => 
-            //     'password' => 
-            //     'position' => 
-            //     'company_name' => 
-            //     'phone' => 
-            //     'web_address' => 
-            //     'mailing_adress' => 
-            //     'city' => 
-            //     'state' => 
-            //     'pin_code' => 
-            //     'status' => 
-            //     'pw' => 
+            $user_exists = DB::connection('mysql')->table('users')->where('email', $user->email)->first();
+            if(!$user_exists){
+                $user_id = DB::connection('mysql')->table('users')->insertGetId([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'password' => $user->password,
+                    'position' => $user->title,
+                    'company_name' => $user->company_name,
+                    'phone' => $user->phone,
+                    'web_address' => $user->web_address,
+                    'mailing_address' => $user->mail_address,
+                    'city' => $user->city,
+                    'state' => $user->state,
+                    'pin_code' => $user->zipcode,
+                    'status' => $user->status === 'Approved' ? 1 : 0,
+                    'pw' => $user->password
 
-            // ]);
+                ]);
+                if($user->socioeconomic_status){
+                    $set_asides = explode(",", $user->socioeconomic_status);
+                    foreach ($set_asides as $set_aside_id) {
+                        $set_aside_old = DB::connection('bidsp4h0_bids_n')->table('tbl_set_aside_status')->where('status_id', $set_aside_id)->first();
+                        if($set_aside_old){
+                            $set_aside_new = DB::connection('mysql')->table('set_asides')->where('set_aside_name', 'like', $set_aside_old->status_name)->first();
+                            if($set_aside_new){
+                                DB::connection('mysql')->table('user_set_asides')->insert([
+                                    'user_id' => $user_id,
+                                    'set_aside_id' => $set_aside_new->set_aside_id
+                                ]);
+                            }
+                        }
+                    }
+                    // Log::info('Set asides:', $set_asides);
+                }
+                $user_subscription_old = DB::connection('bidsp4h0_bids_n')->table('user_subscriptions')->where('user_id', $user->id)->first();
+                if($user_subscription_old){
+                    $subscription_plan = DB::connection('mysql')->table('subscription_plans')->where('month', $user_subscription_old->validity)->first();
+                    if($subscription_plan){
+                        DB::connection('mysql')->table('user_subscriptions')->insert([
+                            'user_id' => $user_id,
+                            'order_id' => $user_subscription_old->order_id,
+                            'payment_method' => $user_subscription_old->payment_method,
+                            'active_status' => $user_subscription_old->active_status,
+                            'validity' => $user_subscription_old->validity,
+                            'valid_from' => $user_subscription_old->valid_from,
+                            'valid_to' => $user_subscription_old->valid_to,
+                            'item_number' => $user_subscription_old->item_number,
+                            'txn_id' => $user_subscription_old->txn_id,
+                            'ipn_track_id' => $user_subscription_old->ipn_track_id,
+                            'payment_gross' => $user_subscription_old->payment_gross,
+                            'currency_code' => $user_subscription_old->currency_code,
+                            'subscription_plan_id' => $subscription_plan->subscription_plan_id,
+                            'payer_email' => $user_subscription_old->payer_email,
+                            'payment_status' => $user_subscription_old->payment_status,
+                            'subscr_month' => $user_subscription_old->subscr_month,
+                            'payment_date' => $user_subscription_old->payment_date
+                        ]);
+                    }
+                }
+
+            }
         }
-
-        // ->where('tdr_region', 1)
-        // ->orderBy('tdr_no')->chunk(100, function ($tenders) {
-        //     $jobData = $tenders->map(function ($tbl) {
-        //         return [
-        //             'tdr_no' => $tbl->tdr_no,
-        //             'tdr_title' => $tbl->tdr_title,
-        //             'tdr_desc' => $tbl->tdr_desc,
-        //             'tdr_opening_date' => $tbl->tdr_opening_date,
-        //             'tdr_posted_date' => $tbl->tdr_posted_date,
-        //             'tdr_expiry_date' => $tbl->tdr_expiry_date,
-        //             'tdr_country_id' => $tbl->tdr_country_id,
-        //             'tdr_state_id' => $tbl->tdr_state_id,
-        //             'tdr_type' => $tbl->tdr_type,
-        //             'tdr_notice' => $tbl->tdr_notice,
-        //             'tdr_category' => $tbl->tdr_category,
-        //             'tdr_agency' => $tbl->tdr_agency,
-        //             'tdr_set_aside_status' => $tbl->tdr_set_aside_status,
-        //             'tdr_naics' => $tbl->tdr_naics,
-        //             'tdr_psc' => $tbl->tdr_psc,
-        //             'tdr_posting_link' => $tbl->tdr_posting_link,
-        //             'tdr_doc_fees' => $tbl->tdr_doc_fees,
-        //             'tdr_type_of_award' => $tbl->tdr_type_of_award,
-        //             'tdr_place_of_performance' => $tbl->tdr_place_of_performance,
-        //             'noticeId' => $tbl->noticeId,
-        //             'description_link' => $tbl->description_link,
-        //             'tdr_documents' => $tbl->tdr_documents,
-        //             'tdr_documents_date' => $tbl->tdr_documents_date,
-        //             'tdr_documents_size' => $tbl->tdr_documents_size,
-        //             'tdr_documents' => $tbl->tdr_documents,
-        //             'tdr_documents_date' => $tbl->tdr_documents_date,
-        //             'tdr_documents_size' => $tbl->tdr_documents_size,
-        //             'tdr_contracting_office_address' => $tbl->tdr_contracting_office_address,
-        //             'point_of_contact' => $tbl->point_of_contact
-        //         ];
-        //     })->toArray();
-
-            // TransferStateTenderJob::dispatch($jobData);
-        // });
-        return response()->json(['message' => 'success'], 200);
     }
 }
